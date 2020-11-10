b98af8522b6dd8eaffe8f44d96e3ac5d4ac7f9af
JestemTrzcinskaLaptop <patrycja.trzcinska125@gmail.com>
2020-11-10 11:30:01 +0100

    Register User - beginning of work

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'))
+router.post('/', [
+  check('firstName', 'Proszę o podanie swojego imienia').not().isEmpty(),
+  check('lastName', 'Proszę o podanie swojego nazwiska.').not().isEmpty(),
+  check('email', 'Proszę o podanie prawidłowego maila.').isEmail(),
+  check(
+    'password',
+    'Słabe hasło! Wprowadź kombinację przynajmniej sześciu liter i cyfr.'
+  ).isLength({ min: 6 }),
+],(req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  
+  res.send('User route');
+});
 
 module.exports = router;
