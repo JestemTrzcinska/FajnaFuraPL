07470cb66b24fe37dc591dbeb0196f6269dd349d
mateusz28011 <mathew28011@gmail.com>
2020-11-17 11:37:25 +0100

    Add POST to cars

--- routes/api/cars.js
+++ routes/api/cars.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 const Car = require('../../models/Car');
 
@@ -16,5 +17,57 @@ router.get('/', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   POST api/cars
+// @desc    Add cars
+// @access  Public
+router.post(
+  '/',
+  [
+    check('brand', 'Proszę o podanie marki samochodu').not().isEmpty(),
+    check('model', 'Proszę o podanie modelu samochodu').not().isEmpty(),
+    check('year', 'Proszę o podanie roku produkcji samochodu').not().isEmpty(),
+    check('sits', 'Proszę o podanie liczby siedzen w samochodzie').not().isEmpty(),
+    check('licenseNumber', 'Proszę o podanie numeru rejestracyjnego samochodu').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { brand, model, year, sits, licenseNumber, about, mileage, conditionOil, conditionTires, tirePressure, multiplerPrice } = req.body;
+
+    try {
+      // See if the car exists
+      let car = await car.findOne({ licenseNumber });
+      if (car) {
+        return res.status(400).json({
+          errors: [{ msg: 'Samochód o podanym numerze rejestracyjnym istnieje.' }],
+        });
+      }
+
+      car = new Car({
+        brand,
+        model,
+        year,
+        sits,
+        licenseNumber,
+        about,
+        mileage,
+        conditionOil,
+        conditionTires,
+        tirePressure,
+        multiplerPrice,
+      });
+
+      await car.save();
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error.');
+    }
+  }
+);
 
 module.exports = router;
