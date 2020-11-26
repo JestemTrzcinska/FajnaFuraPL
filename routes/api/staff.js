176643d83fb523337a8d371dd2e53a9477b5a540
JestemTrzcinskaLaptop <patrycja.trzcinska125@gmail.com>
2020-11-26 13:54:27 +0100

    Staff.findOne -> if(!staff)

--- routes/api/staff.js
+++ routes/api/staff.js
@@ -5,63 +5,58 @@ const { check, validationResult } = require('express-validator');
 const Staff = require('../../models/Staff');
 
 // @route   GET api/staff
-// @desc    Get staff
+// @desc    Get all staffs
 // @access  Public
 router.get('/', async (req, res) => {
-    try {
-      const staffs = await Staff.find();
-  
-      res.json(staffs);
-  
-      if (!staffs) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'Nie ma uprawnień w bazie danych' }] });
-      }
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
+  try {
+    const staffs = await Staff.find();
+
+    if (!staffs) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Nie ma żadnych uprawnień w bazie danych' }] });
     }
+    res.json(staffs);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
-);
+});
 
 // @route   POST api/staff
 // @desc    Post staff
 // @access  Public
 router.post(
-    '/',
-    [
-        check('name','Proszę o podanie uprawnienia').not().isEmpty(),
-    ],
-    async (req,res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { name } = req.body;
-
-        try {
-            // See if the staff exists
-            let staffs = await Staff.findOne({ name });
-            if (staffs) {
-                return res.status(400).json({
-                errors: [{ msg: 'Podane uprawnienie już istnieje.' }],
-                });
-            }
-            
-            staffs = new Staff({
-                name,
-            });
-
-            await staffs.save();
-            res.json(staffs);
-
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error.');
-        }
+  '/',
+  [check('name', 'Proszę o podanie uprawnienia').not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name } = req.body;
+
+    try {
+      // See if the staff exists
+      let staff = await Staff.findOne({ name });
+      if (!staff) {
+        return res.status(400).json({
+          errors: [{ msg: 'Podane uprawnienie nie istnieje.' }],
+        });
+      }
+
+      staff = new Staff({
+        name,
+      });
+
+      await staff.save();
+      res.json(staff);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error.');
+    }
+  }
 );
 
 module.exports = router;
