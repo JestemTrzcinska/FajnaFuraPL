1656326f835e2e9e700242dda870edd336ad8763
mm44419 <mm44419@zut.edu.pl>
2020-12-20 22:46:09 +0100

    MainPage

--- client/src/profil.js
+++ client/src/profil.js
@@ -13,8 +13,13 @@ let changecars = function(el) {
             $(this).removeClass("activecat");
     });
     
-    let temp = $(el).text()+"cat";
-    $("#"+temp).addClass("activecat");
+    let temp = $(el).text();
+    if(temp=="Dane konta")
+        $("#Dane_kontacat").addClass("activecat");
+    else if(temp=="Dane wypożyczenia")
+        $("#Dane_wypozyczeniacat").addClass("activecat");
+    else if(temp=="Dane personalne")
+        $("#Dane_personalnecat").addClass("activecat");
 };
 
 $(".categorybutton").each(function(){
