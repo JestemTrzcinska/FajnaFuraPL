110767f746dd2b0546cd6da6999983c0a8fac5ef
JestemTrzcinskaLaptop <patrycja.trzcinska125@gmail.com>
2020-12-20 15:34:21 +0100

    input and msg caught in login.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -21,7 +21,9 @@ router.post(
       'password',
       'Słabe hasło! Wprowadź kombinację przynajmniej sześciu liter i cyfr.'
     ).isLength({ min: 6 }),
-    check('drivingLicense', 'Proszę o podanie prawidłowego numeru prawa jazdy.').not().isEmpty(),
+    check('drivingLicense', 'Proszę o podanie prawidłowego numeru prawa jazdy.')
+      .not()
+      .isEmpty(),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -36,14 +38,24 @@ router.post(
       let user = await User.findOne({ email });
       if (user) {
         return res.status(400).json({
-          errors: [{ msg: 'Użytkownik o podanym emailu już istnieje.' }],
+          errors: [
+            {
+              input: 'email',
+              msg: 'Użytkownik o podanym emailu już istnieje.',
+            },
+          ],
         });
       }
 
       let user2 = await User.findOne({ drivingLicense });
       if (user2) {
         return res.status(400).json({
-          errors: [{ msg: 'Użytkownik o podanym numerze prawa jazdy już istnieje.' }],
+          errors: [
+            {
+              input: 'drivingLicense',
+              msg: 'Użytkownik o podanym numerze prawa jazdy już istnieje.',
+            },
+          ],
         });
       }
 
