8d43f4a1eb62680dbda329b845431766bf1945e5
kfpolak <72919770+kfpolak@users.noreply.github.com>
2020-12-22 00:56:52 +0100

    Revert "huge structure change"
    This reverts commit b6e706b401194325cdc764b6dc405dea30aa95e4.

---
+++ client/src/loadnavbar.js
@@ -0,0 +1,48 @@
+
+const { set } = require("mongoose");
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+let count = 0;
+let inv = null;
+document.addEventListener('DOMContentLoaded', function(){
+  includeHTML();
+  inv = setInterval(setCurrentPage, 100);
+});
+
+
+function setCurrentPage(){
+  let url = window.location.pathname;
+  url = url.split('/')[1];
+  url = url.split('.')[0];
+  console.log(url);
+  console.log($("#cssmenu a[href='"+url+".html']").parent().html());
+  $("#cssmenu a[href='"+url+".html']").parent().addClass("active");
+  if(count = 5) clearInterval(inv);
+  count++;
+}
