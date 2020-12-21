b6e706b401194325cdc764b6dc405dea30aa95e4
JestemTrzcinskaLaptop <patrycja.trzcinska125@gmail.com>
2020-12-21 19:26:54 +0100

    huge structure change

--- client/src/address/address.js
+++ client/src/address/address.js
@@ -1,23 +1,35 @@
 import 'regenerator-runtime/runtime';
 
 const init = function () {
-  document.getElementById('address_submit').addEventListener('click', address_send);
+  document
+    .getElementById('address_submit')
+    .addEventListener('click', address_send);
 };
 
 const address_send = async function (ev) {
   //Czyszczenie poprzednich błędów
   document.getElementById('street').classList.remove('error');
-  document.getElementById('street').parentElement.classList.remove('error-msg2');
+  document
+    .getElementById('street')
+    .parentElement.classList.remove('error-msg2');
   document.getElementById('building').classList.remove('error');
-  document.getElementById('building').parentElement.classList.remove('error-msg2');
+  document
+    .getElementById('building')
+    .parentElement.classList.remove('error-msg2');
   document.getElementById('apartment').classList.remove('error');
-  document.getElementById('apartment').parentElement.classList.remove('error-msg2');
+  document
+    .getElementById('apartment')
+    .parentElement.classList.remove('error-msg2');
   document.getElementById('zipCode').classList.remove('error');
-  document.getElementById('zipCode').parentElement.classList.remove('error-msg2');
+  document
+    .getElementById('zipCode')
+    .parentElement.classList.remove('error-msg2');
   document.getElementById('city').classList.remove('error');
   document.getElementById('city').parentElement.classList.remove('error-msg2');
   document.getElementById('country').classList.remove('error');
-  document.getElementById('country').parentElement.classList.remove('error-msg2');
+  document
+    .getElementById('country')
+    .parentElement.classList.remove('error-msg2');
 
   //Potwierdzenie opuszczenia strony
   window.onbeforeunload = function (e) {
@@ -52,7 +64,7 @@ const address_send = async function (ev) {
         field.classList.add('error');
         field.parentElement.classList.add('error-msg2');
         field.parentElement.setAttribute('data-errormsg', error.msg);
-      })
+      });
     }
   }
 };
